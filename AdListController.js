f98d35ec55a58abca807777379694cd7f6d35a54
Basilio <basilio.uvinas@gmail.com>
2022-02-17 20:36:30 +0100

    Creada la vista que da formato a la información de API

--- AdListController.js
+++ AdListController.js
@@ -1,15 +1,29 @@
-import AdService from "./AdService.js"
+import AdService from "./AdService.js";
+import { buildAdView } from "./AdView.js";
+
 export class AdListController {
+  constructor(adListElement) {
+    this.adListElement = adListElement;
+  }
 
-    constructor(adListElement) {
-        this.adListElement = adListElement;
-    }
+  async showAds() {
+    let ads;
+    try {
+      ads = await AdService.getAds();
 
-    async showAds() {
-        let ads;
+      //Controlar si está vacio
 
-        ads = await AdService.getAds();
-        console.log('ads', ads);
-        
-    }
+      for (const ad of ads) {
+        const adArticleElement = document.createElement("div");
+        adArticleElement.classList.add(
+          "p-4",
+          "md:w-1/3"
+        );
+        const adTemplate = buildAdView(ad);
+        adArticleElement.innerHTML = adTemplate;
+
+        this.adListElement.appendChild(adArticleElement);
+      }
+    } catch (error) {}
+  }
 }
