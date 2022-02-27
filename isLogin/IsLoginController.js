538ed98c8455905883a9c33b66289c6d05bc5bb1
Basilio <basilio.uvinas@gmail.com>
2022-02-27 19:29:04 +0100

    Feat: Evitar que usuario no logado acceda a zonas privadas

--- isLogin/IsLoginController.js
+++ isLogin/IsLoginController.js
@@ -3,9 +3,11 @@ import { registerService } from "../register/RegisterService.js";
 export class IsLoginController {
   constructor(buttonsVisiblesIfLogged) {
     this.buttonsVisiblesIfLogged = buttonsVisiblesIfLogged;
+    this.isPrivateArea();
   }
 
-  checkUserLogin() {
+  // Cambio de header según tipo de Usuario (Guest o Logging)
+  checkUserLoginChangeHeader() {
     const loggedUserToken = registerService.getLoggedUser();
 
     if (loggedUserToken) {
@@ -24,6 +26,7 @@ export class IsLoginController {
     }
   }
 
+  // Cerrar sesión
   logout() {
     const logout = this.buttonsVisiblesIfLogged.querySelector(".login .logout");
     logout.addEventListener("click", (e) => {
@@ -35,5 +38,42 @@ export class IsLoginController {
       }
       window.location.href = "/";
     });
+  }
+
+  // Si no está logado, redirecciona
+  isLogin() {
+    const loggedUserToken = registerService.getLoggedUser();
+
+    if (!loggedUserToken) {
+      alert("Tienes que loguearte para acceder.");
+      setTimeout(() => {
+        window.location.href = "/";
+      }, 500);
+      return;
+    }
+  }
+
+  // Limpiar Url
+  checkUrl() {
+    const searchUrl = window.location;
+    const removeBar = searchUrl.pathname.split("/");
+    const removeExtension = removeBar[1].split(".html");
+    return removeExtension[0];
+  }
+
+  // Listado de areas privadas
+  privateArea() {
+    const privateArea = ["create-ad"];
+    return privateArea;
+  }
+
+  // Averigua Url, Si es zona privada y si el usuario está logado
+  isPrivateArea() {
+    const url = this.checkUrl();
+    const privateArea = this.privateArea();
+
+    if (privateArea.includes(url)) {
+      this.isLogin()
+    }
   }
 }
